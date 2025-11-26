51eb4546be35d138a07f3098e0f07b17000e7733
Lanz Ruiz <lanzruizdesigns@gmail.com>
2025-11-26 18:59:36 +0800

    finishing the project

--- src/index.ts
+++ src/index.ts
@@ -32,10 +32,19 @@ function handleSubmit(e: SubmitEvent) {
   input.value = "";
 }
 
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
 function renderTodo(todo: Todo) {
   const newLI = document.createElement("LI");
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
+  checkbox.addEventListener("change", function () {
+    todo.completed = checkbox.checked;
+    saveTodos();
+  });
   newLI.append(todo.text);
   newLI.append(checkbox);
   list.append(newLI);
