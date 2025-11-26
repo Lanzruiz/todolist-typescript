aeb22a7fed5e0dcfb5f85ad2a0cf27f3d7d1c21d
Lanz Ruiz <lanzruizdesigns@gmail.com>
2025-11-26 11:39:21 +0800

    working the todo listing

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,24 @@ console.log("Hello");
 const btn = document.getElementById("btn")!;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;
 
-// function handleSubmit(e: SubmitEvent) {
-//   e.preventDefault();
-//   console.log("Submitted");
-// }
-
-// form.addEventListener("submit", handleSubmit);
-
-form.addEventListener("submit", function (e) {
+function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  console.log("Submitted!");
-});
+
+  const newTodoText = input.value;
+  const newLI = document.createElement("LI");
+  newLI.append(newTodoText);
+  list.append(newLI);
+  input.value = "";
+}
+
+form.addEventListener("submit", handleSubmit);
+
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   console.log("Submitted!");
+// });
 
 // btn.addEventListener("click", function () {
 //   alert(input.value);
