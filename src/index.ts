3d637b40ed999eb10faa49e90a0c45ea60437196
Lanz Ruiz <lanzruizdesigns@gmail.com>
2025-11-26 13:41:00 +0800

    Implement interface and array for the list

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 console.log("Hello");
 
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const todos: Array<Todo> = [];
+
 const btn = document.getElementById("btn")!;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
@@ -7,12 +14,24 @@ const list = document.getElementById("todolist")!;
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
 
+  todos.push(newTodo);
+  renderTodo(newTodo);
   const newTodoText = input.value;
-  const newLI = document.createElement("LI");
-  newLI.append(newTodoText);
-  list.append(newLI);
   input.value = "";
+}
+
+function renderTodo(todo: Todo) {
+  const newLI = document.createElement("LI");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newLI.append(todo.text);
+  newLI.append(checkbox);
+  list.append(newLI);
 }
 
 form.addEventListener("submit", handleSubmit);
